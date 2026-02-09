cc7716ca5043a28b1d1b41abb437b69926e50109
Peter Hraško <phr@whitestein.com>
2026-02-09 10:10:22 +0100

    ➕ permutations generator (and tests)

---
+++ CombiGen.Tests/GetCombinations_Tests.cs
@@ -0,0 +1,140 @@
+namespace NoP77svk.CombiGen.Tests;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NoP77svk.CombiGen;
+
+public class GetCombinations_Tests
+{
+    private readonly char[] _testElements = ['a', 'b', 'c', 'd', 'e'];
+
+    [Test]
+    public void GetCombinations_SingleElementTuple_Returns_SingleElementCollectionsOfAllElements()
+    {
+        // Arrange
+        int expectedResultsCount = CombiMath.BinomialCoefficient(_testElements.Length, 1);
+
+        IList<IList<KeyValuePair<int, char>>> expectedResult = new List<IList<KeyValuePair<int, char>>>();
+        for (int i = 0; i < _testElements.Length; i++)
+        {
+            KeyValuePair<int, char>[] expectedVariaton = [new(i, _testElements[i])];
+            expectedResult.Add(expectedVariaton);
+        }
+
+        // Act
+        var actualResult = CombiGenerator.GetCombinations(_testElements, 1)
+            .Select(x => x.OrderBy(kvp => kvp.Key).ToArray())
+            .ToArray();
+
+        // Assert
+        Assert.That(actualResult.Length, Is.EqualTo(expectedResultsCount));
+        Assert.That(actualResult, Is.EquivalentTo(expectedResult));
+    }
+
+    [Test]
+    public void GetCombinations_DoubleElementTuple_Returns_ProperDoublesOfElements()
+    {
+        // Arrange
+        int expectedResultsCount = CombiMath.BinomialCoefficient(_testElements.Length, 2);
+
+        IList<IList<KeyValuePair<int, char>>> expectedResult = new List<IList<KeyValuePair<int, char>>>();
+        for (int i = 0; i < _testElements.Length - 1; i++)
+        {
+            for (int j = i + 1; j < _testElements.Length; j++)
+            {
+                KeyValuePair<int, char>[] expectedCombination = [new(i, _testElements[i]), new(j, _testElements[j])];
+                expectedResult.Add(expectedCombination);
+            }
+        }
+
+        // Act
+        var actualResult = CombiGenerator.GetCombinations(_testElements, 2)
+            .Select(x => x.OrderBy(kvp => kvp.Key).ToArray())
+            .ToArray();
+
+        // Assert
+        Assert.That(actualResult.Length, Is.EqualTo(expectedResultsCount));
+        Assert.That(actualResult, Is.EquivalentTo(expectedResult));
+    }
+
+    [Test]
+    public void GetCombinations_FullLengthTuple_Returns_ProperAllElementsTuples()
+    {
+        // Arrange
+        int expectedResultsCount = 1;
+
+        IList<IList<KeyValuePair<int, char>>> expectedResult = new List<IList<KeyValuePair<int, char>>>();
+        for (int a = 0; a < _testElements.Length; a++)
+        {
+            for (int b = a + 1; b < _testElements.Length; b++)
+            {
+                if (b == a)
+                {
+                    continue;
+                }
+
+                for (int c = b + 1; c < _testElements.Length; c++)
+                {
+                    if (c == a || c == b)
+                    {
+                        continue;
+                    }
+
+                    for (int d = c + 1; d < _testElements.Length; d++)
+                    {
+                        if (d == a || d == b || d == c)
+                        {
+                            continue;
+                        }
+
+                        for (int e = d + 1; e < _testElements.Length; e++)
+                        {
+                            if (e == a || e == b || e == c || e == d)
+                            {
+                                continue;
+                            }
+
+                            KeyValuePair<int, char>[] expectedCombination = [
+                                new(a, _testElements[a]),
+                                new(b, _testElements[b]),
+                                new(c, _testElements[c]),
+                                new(d, _testElements[d]),
+                                new(e, _testElements[e])
+                            ];
+
+                            expectedResult.Add(expectedCombination);
+                        }
+                    }
+                }
+            }
+        }
+
+        // Act
+        var actualResult = CombiGenerator.GetCombinations(_testElements, _testElements.Length)
+            .Select(x => x.OrderBy(kvp => kvp.Key).ToArray())
+            .ToArray();
+
+        // Assert
+        Assert.That(actualResult.Length, Is.EqualTo(expectedResultsCount));
+        Assert.That(actualResult, Is.EquivalentTo(expectedResult));
+    }
+
+    [Test]
+    public void GetCombinations_FullLengthTuple_Returns_TheInputInArbitraryOrder()
+    {
+        // Arrange
+        int expectedResultsCount = 1;
+        IList<IList<KeyValuePair<int, char>>> expectedResult = [_testElements.Select((v, i) => new KeyValuePair<int, char>(i, v)).ToArray()];
+
+        // Act
+        var actualResult = CombiGenerator.GetCombinations(_testElements, _testElements.Length)
+            .Select(x => x.OrderBy(kvp => kvp.Key).ToArray())
+            .ToArray();
+
+        // Assert
+        Assert.That(actualResult.Length, Is.EqualTo(expectedResultsCount));
+        Assert.That(actualResult, Is.EquivalentTo(expectedResult));
+    }
+}
--- CombiGen/CombiGenerator.cs
+++ CombiGen/CombiGenerator.cs
@@ -12,15 +12,15 @@ namespace NoP77svk.CombiGen
             => GetCombinations(values.ToArray(), tupleSize);
 
         public static IEnumerable<IEnumerable<KeyValuePair<int, T>>> GetCombinations<T>(this IList<T> values, int tupleSize)
-            => throw new NotImplementedException();
+            => InternalGetVariations(values, tupleSize, new Stack<int>(), 0, false);
 
         public static IEnumerable<IEnumerable<KeyValuePair<int, T>>> GetVariations<T>(this IEnumerable<T> values, int tupleSize)
             => GetVariations(values.ToArray(), tupleSize);
 
         public static IEnumerable<IEnumerable<KeyValuePair<int, T>>> GetVariations<T>(this IList<T> values, int tupleSize)
-            => InternalGetVariations(values, tupleSize, new Stack<int>(), () => 0);
+            => InternalGetVariations(values, tupleSize, new Stack<int>(), 0, true);
 
-        private static IEnumerable<IEnumerable<KeyValuePair<int, T>>> InternalGetVariations<T>(IList<T> values, int tupleSize, Stack<int> usedInputIndices, Func<int> startIndexSelector)
+        private static IEnumerable<IEnumerable<KeyValuePair<int, T>>> InternalGetVariations<T>(IList<T> values, int tupleSize, Stack<int> usedInputIndices, int startIndex, bool enableOrderedTuples)
         {
             if (tupleSize <= 0)
             {
@@ -31,9 +31,13 @@ namespace NoP77svk.CombiGen
                 yield break;
             }
 
+            if (startIndex >= values.Count)
+            {
+                yield break;
+            }
+
             HashSet<int> usedInputIndicesIndexed = usedInputIndices.ToHashSet();
 
-            int startIndex = startIndexSelector();
             for (int i = startIndex; i < values.Count; i++)
             {
                 if (usedInputIndicesIndexed.Contains(i))
@@ -42,7 +46,8 @@ namespace NoP77svk.CombiGen
                 }
 
                 usedInputIndices.Push(i);
-                var oneMoreElementVariations = InternalGetVariations(values, tupleSize - 1, usedInputIndices, startIndexSelector);
+                int newStartIndex = enableOrderedTuples ? 0 : i + 1;
+                var oneMoreElementVariations = InternalGetVariations(values, tupleSize - 1, usedInputIndices, newStartIndex, enableOrderedTuples);
 
                 foreach (var variation in oneMoreElementVariations)
                 {
