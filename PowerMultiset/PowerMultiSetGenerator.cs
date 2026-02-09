1d051ef3e29ea5644055ba33dc504267ac0e629f
Peter Hraško <phr@whitestein.com>
2026-02-09 14:35:44 +0100

    🔧 "element order matters" switched from boolean to an enum

--- PowerMultiset/PowerMultiSetGenerator.cs
+++ PowerMultiset/PowerMultiSetGenerator.cs
@@ -28,7 +28,7 @@ namespace NoP77svk.PowerMultiset
         ///     </list></para>
         /// </returns>
         public static IEnumerable<IEnumerable<KeyValuePair<int, T>>> Permute<T>(this IList<T> values)
-            => values.GenerateSubMultiSet(values.Count, true);
+            => values.GenerateSubMultiSet(values.Count, SubMultiSetElementOrder.Matters);
 
         /// <summary>
         /// Generate all <paramref name="tupleSize"/>-element long combinations for the given multiset <paramref name="values"/>.
@@ -58,7 +58,7 @@ namespace NoP77svk.PowerMultiset
         ///     </list></para>
         /// </returns>
         public static IEnumerable<IEnumerable<KeyValuePair<int, T>>> Combine<T>(this IList<T> values, int tupleSize)
-            => values.GenerateSubMultiSet(tupleSize, false);
+            => values.GenerateSubMultiSet(tupleSize, SubMultiSetElementOrder.DoesNotMatter);
 
         /// <summary>
         /// Generate all <paramref name="tupleSize"/>-element long variations for the given multiset <paramref name="values"/>.
@@ -88,16 +88,14 @@ namespace NoP77svk.PowerMultiset
         ///     </list></para>
         /// </returns>
         public static IEnumerable<IEnumerable<KeyValuePair<int, T>>> Vary<T>(this IList<T> values, int tupleSize)
-            => values.GenerateSubMultiSet(tupleSize, true);
+            => values.GenerateSubMultiSet(tupleSize, SubMultiSetElementOrder.Matters);
 
         /// <summary>
         /// Generate all sub-multi-sets (e.g. variations or combinations of all possible tuple sizes) for the given multiset <paramref name="values"/>.
         /// </summary>
         /// <param name="values">The input multiset.</param>
-        /// <param name="elementOrderMatters"><list type="bullet">
-        ///     <item><c>true</c> = each resulting multiset is a variation from the input <paramref name="values"/> multiset,</item>
-        ///     <item><c>false</c> = each resulting multiset is a combination from the input <paramref name="values"/> multiset,</item>
-        /// </list></param>
+        /// <param name="elementOrder">If it "matters", then each resulting multiset is a variation from the input <paramref name="values"/> multiset,
+        ///     otherwise each resulting multiset is a combination from the input <paramref name="values"/> multiset,</param>
         /// <typeparam name="T">The element type of the input multiset.</typeparam>
         /// <returns>
         ///     <para>Enumerable of all possible submultisets of the input multiset. Each variation is a collection of key/value pairs where <list type="bullet">
@@ -105,17 +103,15 @@ namespace NoP77svk.PowerMultiset
         ///         <item>value = the original <paramref name="values"/> multiset element</item>
         ///     </list></para>
         /// </returns>
-        public static IEnumerable<IEnumerable<KeyValuePair<int, T>>> GeneratePowerMultiSet<T>(this IEnumerable<T> values, bool elementOrderMatters)
-            => GeneratePowerMultiSet(values.ToArray(), elementOrderMatters);
+        public static IEnumerable<IEnumerable<KeyValuePair<int, T>>> GeneratePowerMultiSet<T>(this IEnumerable<T> values, SubMultiSetElementOrder elementOrder)
+            => GeneratePowerMultiSet(values.ToArray(), elementOrder);
 
         /// <summary>
         /// Generate all sub-multi-sets (e.g. variations or combinations of all possible tuple sizes) for the given multiset <paramref name="values"/>.
         /// </summary>
         /// <param name="values">The input multiset.</param>
-        /// <param name="elementOrderMatters"><list type="bullet">
-        ///     <item><c>true</c> = each resulting multiset is a variation from the input <paramref name="values"/> multiset,</item>
-        ///     <item><c>false</c> = each resulting multiset is a combination from the input <paramref name="values"/> multiset,</item>
-        /// </list></param>
+        /// <param name="elementOrder">If it "matters", then each resulting multiset is a variation from the input <paramref name="values"/> multiset,
+        ///     otherwise each resulting multiset is a combination from the input <paramref name="values"/> multiset,</param>
         /// <typeparam name="T">The element type of the input multiset.</typeparam>
         /// <returns>
         ///     <para>Enumerable of all possible submultisets of the input multiset. Each variation is a collection of key/value pairs where <list type="bullet">
@@ -123,29 +119,29 @@ namespace NoP77svk.PowerMultiset
         ///         <item>value = the original <paramref name="values"/> multiset element</item>
         ///     </list></para>
         /// </returns>
-        public static IEnumerable<IEnumerable<KeyValuePair<int, T>>> GeneratePowerMultiSet<T>(this IList<T> values, bool elementOrderMatters)
+        public static IEnumerable<IEnumerable<KeyValuePair<int, T>>> GeneratePowerMultiSet<T>(this IList<T> values, SubMultiSetElementOrder elementOrder)
         {
             var result = Enumerable.Empty<IEnumerable<KeyValuePair<int, T>>>();
 
             for (int i = 0; i <= values.Count; i++)
             {
-                var singleTupleSizeResult = values.GenerateSubMultiSet(i, elementOrderMatters);
+                var singleTupleSizeResult = values.GenerateSubMultiSet(i, elementOrder);
                 result = result.Concat(singleTupleSizeResult);
             }
 
             return result;
         }
 
-        private static IEnumerable<IEnumerable<KeyValuePair<int, T>>> GenerateSubMultiSet<T>(this IList<T> values, int tupleSize, bool elementOrderMatters)
+        private static IEnumerable<IEnumerable<KeyValuePair<int, T>>> GenerateSubMultiSet<T>(this IList<T> values, int tupleSize, SubMultiSetElementOrder elementOrder)
             => InternalGenerateSubMultiSet(
                 values: values,
                 tupleSize: tupleSize,
                 usedInputIndices: new Stack<int>(),
                 startIndex: 0,
-                elementOrderMatters: elementOrderMatters
+                elementOrder: elementOrder
             );
 
-        private static IEnumerable<IEnumerable<KeyValuePair<int, T>>> InternalGenerateSubMultiSet<T>(IList<T> values, int tupleSize, Stack<int> usedInputIndices, int startIndex, bool elementOrderMatters)
+        private static IEnumerable<IEnumerable<KeyValuePair<int, T>>> InternalGenerateSubMultiSet<T>(IList<T> values, int tupleSize, Stack<int> usedInputIndices, int startIndex, SubMultiSetElementOrder elementOrder)
         {
             if (tupleSize <= 0)
             {
@@ -171,8 +167,14 @@ namespace NoP77svk.PowerMultiset
                 }
 
                 usedInputIndices.Push(i);
-                int newStartIndex = elementOrderMatters ? 0 : i + 1;
-                var tuplesWithOneMoreElement = InternalGenerateSubMultiSet(values, tupleSize - 1, usedInputIndices, newStartIndex, elementOrderMatters);
+                int newStartIndex = elementOrder switch
+                {
+                    SubMultiSetElementOrder.Matters => 0,
+                    SubMultiSetElementOrder.DoesNotMatter => i + 1,
+                    _ => throw new ArgumentOutOfRangeException(nameof(elementOrder), elementOrder, "Unrecognized element order")
+                };
+
+                var tuplesWithOneMoreElement = InternalGenerateSubMultiSet(values, tupleSize - 1, usedInputIndices, newStartIndex, elementOrder);
 
                 foreach (var resultTuple in tuplesWithOneMoreElement)
                 {
@@ -182,5 +184,11 @@ namespace NoP77svk.PowerMultiset
                 usedInputIndices.Pop();
             }
         }
+    }
+
+    public enum SubMultiSetElementOrder
+    {
+        Matters,
+        DoesNotMatter
     }
 }
